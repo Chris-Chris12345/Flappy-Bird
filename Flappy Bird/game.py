1eac3ab6cb5626e8a70fffc09903c2b70cb45049
Chris-Chris12345 <christian.amirza@outlook.com>
2024-11-03 10:55:55 +0100

    game.py

--- Flappy Bird/game.py
+++ Flappy Bird/game.py
@@ -66,4 +66,77 @@ class Button():
         screen.blit(self.image, (self.rect.x, self.rect.y))
         return action
 
+class Bird(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+        self.index = 0
+        self.counter = 0
+        for i in range(1,3):
+            img = pygame.image.load(f"bird{i}.png")
+            self.images.append(img)
+        
+        self.image = self.images[self.index]
+        self.rect = self.image.get_rect
+        self.rect.center = [x,y]
+        self.vel = 0
+        self.click = False
+
+    def update(self):
+        if flying == True:
+            #apply gravity
+            self.vel += 0.5
+            if self.vel > 8:
+                self.vel = 8
+            if self.rect.bottom < 768:
+                self.rect.y += int(self.vel)
+        
+        if game_over == False:
+            #jump effect
+            if pygame.mouse.get_pressed()[0] == 1 and self.click == False:
+                self.click = True
+                self.vel -= 10
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.click = False
+
+            #animation
+            flap_cooldown = 5
+            self.counter += 1
+            if self.counter > flap_cooldown:
+                self.counter = 0
+                self.index += 1
+                if self.index >= len(self.images):
+                    self.index = 0
+                    self.image = self.images[self.index]
+
+            #rotation
+            self.image = pygame.transform.rotate(self.images[self.index], self.vel * -2)
+        else:
+            self.image = pygame.transform.rotate(self.images[self.index], -90)
+
+def reset_game():
+    pipe_group.empty()
+    flappy.rect.x = 100
+    flappy.rect.y = int(screen_height/2)
+    score = 0
+    return score
+
+pipe_group = pygame.sprite.Group()
+bird_group = pygame.sprite.Group()
+
+flappy = Bird(100,int(screen_height/2))
+restart_button = Button(screen_width/2, screen_height/2, restart)
+
+while 1:
+    screen.blit(bg,(0,0))
+
+    pipe_group.draw(screen)
+    bird_group.draw(screen)
+    bird_group.update()
+
+    if game_over == True:
+        if restart_button.draw():
+            game_over = False
+            score = reset_game()
+            
 
